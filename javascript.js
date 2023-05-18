7db471494c8fc8c3597eebb37f666f1472e825f7
James Otwane <127046977+ja259@users.noreply.github.com>
2023-05-17 21:16:25 -0400

    Add javascript.js

---
+++ javascript.js
@@ -0,0 +1,9 @@
+/* javascript.js */
+
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
